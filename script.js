c80021e896bb13cb1d20a0f76b18273ef89c98e2
Robotik App <terrified.cloth@gmail.com>
2024-12-07 19:55:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -24,9 +24,9 @@ export default function Widget() {
             <section className="my-12">
                 <h2 className="text-2xl font-semibold mb-4">Empowering Indonesia’s entrepreneurs and industry leaders</h2>
                 <div className="flex flex-wrap justify-center gap-4">
-                    <img src="https://drive.google.com/uc?export=view&id=1WmGLVNjE1bTUsefZoJt8ZjhWpB74ARGs" alt="Katalog Sepatu Nasional" className="w-16 h-16" />
-                    <img src="https://drive.google.com/uc?export=view&id=12xuDxpxunXcL9e5nb3zGda2Nrn0rMDcg" alt="The Pavilion" className="w-16 h-16" />
-                    <img src="https://drive.google.com/uc?export=view&id=1tem3n7KM8I71qVJmevgusjNavrbzX5gt" alt="PT Cipta Adi Surya" className="w-16 h-16" />
+                    <img src="https://github.com/robotikapp/home/blob/main/robotik-asset/Copy%20of%20Logo%20Toko.png" alt="Katalog Sepatu Nasional" className="w-16 h-16" />
+                    <img src="https://github.com/robotikapp/home/blob/main/robotik-asset/Copy%20of%20Logo%20Pavilion.png" alt="The Pavilion" className="w-16 h-16" />
+                    <img src="https://github.com/robotikapp/home/blob/main/robotik-asset/Logo%20PT%20CIPTA%20ADI%20SURYA%20FIX.png" alt="PT Cipta Adi Surya" className="w-16 h-16" />
                 </div>
             </section>
             <footer className="bg-accent text-accent-foreground py-6 text-center">
